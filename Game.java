311976e93c7dc44f478cd465fc50d98f5563ee81
Prabhav <abcd@email.com>
2025-11-30 12:32:53 +0530

    JFrame_setup

---
+++ Game.java
@@ -0,0 +1,20 @@
+//class first{
+//   public static void main(String[] args) {
+//    System.out.println("Hello bitches");
+//}
+//}
+
+import javax.swing.*;
+
+public class Game {
+     public static void main(String[] args) {
+
+        JFrame frame = new JFrame("New Game");  
+        frame.setVisible(true);
+        frame.setSize(1280, 720);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     }
+    
+}
